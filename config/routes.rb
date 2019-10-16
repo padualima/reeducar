21964a64679445ad7fd901ea6d50215a14aef475
Pádua Lima <padualima.dev@gmail.com>
2019-10-16 15:35:20 -0300

    session login initall views settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :recipes
   devise_for :users
   resources :categories
-  get 'homes/index'
+  root 'homes#index'
   get 'homes/categories'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
